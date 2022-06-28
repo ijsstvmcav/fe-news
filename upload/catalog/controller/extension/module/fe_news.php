ac9b965e94349c2d0f3093bb454c4f502f118c41
fw <fw@fw>
2022-06-28 14:42:16 +0600

    Add news list page

--- upload/catalog/controller/extension/module/fe_news.php
+++ upload/catalog/controller/extension/module/fe_news.php
@@ -5,9 +5,16 @@ class ControllerExtensionModuleFeNews extends Controller {
     public function getNewsList() {
         $this->load->model('extension/module/fe_news_information');
         $informations = $this->model_extension_module_fe_news_information->getNewsInformations();
+        $informations = array_map(function($it) {
+            $it['description'] = html_entity_decode($it['description'], ENT_QUOTES, 'UTF-8');
+            return $it;
+        }, $informations);
 
         $data = [];
+        $data['link_information'] = $this->url->link('information/information', '', true) . '&information_id=';
         $data['informations'] = $informations;
+        $data['header'] = $this->load->controller('fe/common/header');
+        $data['footer'] = $this->load->controller('fe/common/footer');
         return $this->load->view('extension/module/fe_news', $data);
     }
 
